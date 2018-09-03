bb287cc994ca58fafcf7c1bb0c486cfdd6b21af5
Cristian Garcia <cgarcia.e88@gmail.com>
2018-09-03 11:55:13 -0500

    update to version 0.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = [
 
 setup(
     name = "pypeln",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Cristian Garcia",
     author_email = "cgarcia.e88@gmail.com",
     description = (""),

f06e98116d7a9dc7042576b1584f5e6b034c63d3
Cristian Garcia <cgarcia.e88@gmail.com>
2018-09-09 14:30:42 -0600

    readme + tests

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pypeln
+`pypeln` is a python library that lets you easily create concurrent data pipelines in a functional style. 
+
+
+## Contributors
+* [cgarciae](https://github.com/cgarciae)
+* [davidnet](https://github.com/davidnet)
--- tests/test_io.py
+++ tests/test_io.py
@@ -1,8 +1,13 @@
 
 import hypothesis as hp
 from hypothesis import strategies as st
+import cytoolz as cz
 
 from pypeln import io
+
+############
+# map
+############
 
 @hp.given(numbers = st.lists(st.integers()))
 def test_map_id(numbers):
@@ -37,6 +42,48 @@ def test_map_square_workers(numbers):
     numbers_py = list(numbers_py)
 
     numbers_pl = io.map(lambda x: x ** 2, numbers, workers=2)
+    numbers_pl = list(numbers_pl)
+
+    assert sorted(numbers_pl) == sorted(numbers_py)
+
+
+############
+# flat_map
+############
+
+@hp.given(numbers = st.lists(st.integers()))
+def test_flat_map_square(numbers):
+
+    def _generator(x):
+        yield x
+        yield x + 1
+        yield x + 2
+    
+    numbers_py = map(lambda x: x ** 2, numbers)
+    numbers_py = cz.mapcat(_generator, numbers_py)
+    numbers_py = list(numbers_py)
+
+    numbers_pl = io.map(lambda x: x ** 2, numbers)
+    numbers_pl = io.flat_map(_generator, numbers_pl)
+    numbers_pl = list(numbers_pl)
+
+    assert numbers_pl == numbers_py
+
+
+@hp.given(numbers = st.lists(st.integers()))
+def test_flat_map_square_workers(numbers):
+
+    def _generator(x):
+        yield x
+        yield x + 1
+        yield x + 2
+    
+    numbers_py = map(lambda x: x ** 2, numbers)
+    numbers_py = cz.mapcat(_generator, numbers_py)
+    numbers_py = list(numbers_py)
+
+    numbers_pl = io.map(lambda x: x ** 2, numbers)
+    numbers_pl = io.flat_map(_generator, numbers_pl, workers=3)
     numbers_pl = list(numbers_pl)
 
     assert sorted(numbers_pl) == sorted(numbers_py)

db031cb4a676672d2c199a4616652ce2433c1784
Cristian Garcia <cgarcia.e88@gmail.com>
2018-09-20 10:29:08 -0500

    improve io exception handling + examples

---
+++ examples/io_downloads.py
@@ -0,0 +1,26 @@
+from aiohttp import ClientSession
+from pypeln import io
+import asyncio
+import sys
+
+async def fetch(url, session):
+    async with session.get(url) as response:
+        return await response.read()
+
+async def main():
+    r = 10
+    url = "http://google.com"
+
+    # r = int(sys.argv[1])
+    # url = "http://localhost:8080/{}"
+
+    async with ClientSession() as session:
+        data = range(r)
+        await io.each(lambda i: fetch(url, session), data, workers=1000, run = False)
+    
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+
+print("Finish")

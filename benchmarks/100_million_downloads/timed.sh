d1e1e15e4bf3ec73df49da5f88d03387692c0dd2
Cristian Garcia <cgarcia.e88@gmail.com>
2018-09-21 10:46:53 -0500

    clean timed

--- benchmarks/100_million_downloads/timed.sh
+++ benchmarks/100_million_downloads/timed.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-python benchmarks/server.py &
+python server.py &
+
 sleep 1 # Wait for server to start
 
 /usr/bin/time --format "Memory usage: %MKB\tTime: %e seconds\tCPU usage: %P" "$@"
-# /usr/bin/time -v "$@"
-
-# %e Elapsed real (wall clock) time used by the process, in seconds.
-# %M Maximum resident set size of the process in Kilobytes.
 
 kill %1

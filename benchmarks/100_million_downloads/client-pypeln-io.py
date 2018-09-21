6b4cbd7f43b0761aa8720eb65fe823c85c2d40ad
Cristian Garcia <cgarcia.e88@gmail.com>
2018-09-21 11:06:16 -0500

    clean client pypeln io

--- benchmarks/100_million_downloads/client-pypeln-io.py
+++ benchmarks/100_million_downloads/client-pypeln-io.py
@@ -2,30 +2,28 @@
 
 from aiohttp import ClientSession
 import asyncio
+import sys
 from pypeln import io
-import fire
 
+limit = 1000
 
 async def fetch(url, session):
     async with session.get(url) as response:
         return await response.read()
 
-async def _main(url, total_requests, workers):
+async def _main(url, total_requests):
 
     async with ClientSession() as session:
 
         await io.each(
             lambda i: fetch(url.format(i), session), 
             range(total_requests), 
-            workers = workers, 
+            workers = limit, 
             run = False,
         )
     
 
-def main(total_requests, workers = 1000):
-    url = "http://localhost:8080/{}"
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(_main(url, total_requests, workers))
 
-if __name__ == '__main__':
-    fire.Fire(main)
+url = "http://localhost:8080/{}"
+loop = asyncio.get_event_loop()
+loop.run_until_complete(_main(url, int(sys.argv[1])))

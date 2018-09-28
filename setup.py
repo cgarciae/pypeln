8ae42ec7d1a01b7a6f51d0e09a2faac19f06e2a9
Cristian Garcia <cgarcia.e88@gmail.com>
2018-09-28 13:05:34 -0500

    0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = [
 
 setup(
     name = "pypeln",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "Cristian Garcia",
     author_email = "cgarcia.e88@gmail.com",
     description = (""),

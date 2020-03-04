54085a63bea1dbbc50a1af52f4ab6a62d83f6e45
Cristian Garcia <cgarcia.e88@gmail.com>
2020-03-04 13:16:24 -0500

    add sync docs

--- pypeln/sync/api.py
+++ pypeln/sync/api.py
@@ -1,5 +1,8 @@
 """
 The `sync` module follows same API as the rest of the modules but runs the code synchronously using normal python generators. This module is intended to be used for debugging purposes as synchronous code tends to be easier to debug than concurrent code in Python (e.g. vscode's debugger doesn't work well (if at all) with the multiprocessing and threading modules).
+
+Common arguments such as `workers` and `maxsize` are accepted by this module's 
+functions for API compatibility purposes but are ignored.
 """
 import typing
 from threading import Thread

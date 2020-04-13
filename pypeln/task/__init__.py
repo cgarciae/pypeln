cbf5f1abb700cc0a50ae03c1e88a3392b5f0e8b6
Lars-Dominik Braun <lars@6xq.net>
2020-04-13 10:22:19 +0200

    Re-export to_iterator

--- pypeln/task/__init__.py
+++ pypeln/task/__init__.py
@@ -1,2 +1,2 @@
-from .api import Stage, concat, each, filter, flat_map, from_iterable, map, run, ordered
+from .api import Stage, concat, each, filter, flat_map, from_iterable, map, run, ordered, to_iterable
 from .utils import TaskPool, get_namespace

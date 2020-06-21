904819b439033f43192bdd6fb68de8f536036b5d
Cristian Garcia <cgarcia.e88@gmail.com>
2020-06-21 15:06:45 -0500

    lower version python requirement

--- pypeln/__init__.py
+++ pypeln/__init__.py
@@ -1,9 +1,13 @@
+import sys
+
 from . import sync
 from . import thread
 from . import process
-from . import task
 from .utils import Element
-
 from .utils import BaseStage
+
+if sys.version_info >= (3, 7):
+    from . import task
+
 
 __version__ = "0.3.0"

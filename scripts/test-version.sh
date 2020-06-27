487d166db06b2c06fb35ac9ed772c55d9dfeaaed
David Cardozo <david.cardozo@me.com>
2020-06-27 11:48:41 -0500

    [Feat] Apply this to get a test Dockerfile. (#44)
    * [Feat] Apply this to get a test Dockerfile.
    
    We map a volume that contains the source code for testing in a safe enviroment.
    
    We can support both docker and podman in the new script.
    
    * [Fix] Typo error in the image tag.
    
    I made the mistake of registering a container with pipeline typo.
    
    * test-all-versions
    
    * add dockerignore
    
    * add -s to tests
    
    * add contributor
    
    Co-authored-by: Cristian Garcia <cgarcia.e88@gmail.com>

---
+++ scripts/test-version.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# run-test - A script to run tests pypeln in a container
+# can recive an optional declaring python version
+set -e
+
+kPYTHON_VERSIONS='^[3]\.[0-9]{1,2}$'
+kDEFAULT_VERSION=3.8
+
+
+container_runner () {
+    if [[ -z "$1" ]]; then
+        py_version="$kDEFAULT_VERSION"
+    else
+        py_version=$1
+    fi
+
+    if hash podman 2>/dev/null; then
+        podman build --build-arg PY_VERSION="$py_version" -t pypeln .
+        podman run -it --rm  -v "$(pwd)":/usr/src/app:Z pypeln:latest
+    else
+        docker build --build-arg PY_VERSION="$py_version" -t pypeln .
+        docker run -it --rm  -v "$(pwd)":/usr/src/app:Z pypeln:latest
+    fi
+}
+
+if [[ $1 =~ $kPYTHON_VERSIONS ]] || [[ -z "$1" ]]; then
+    container_runner "$1"
+else
+    echo "Check python version"
+fi

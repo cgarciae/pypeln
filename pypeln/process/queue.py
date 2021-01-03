0a26f6a298e77c12322d8eb6a01695aa5e6128e2
Cristian Garcia <cgarcia.e88@gmail.com>
2021-01-02 19:33:36 -0500

    flacky

--- pypeln/process/queue.py
+++ pypeln/process/queue.py
@@ -65,12 +65,12 @@ class IterableQueue(Queue, tp.Generic[T], tp.Iterable[T]):
             yield x
 
     def is_done(self):
-        is_empty = self.empty()
 
         with self.namespace:
-            return self.namespace.force_stop or (
-                self.namespace.remaining <= 0 and is_empty
-            )
+            force_stop = self.namespace.force_stop
+            remaining = self.namespace.remaining
+
+        return force_stop or (remaining <= 0 and self.empty())
 
     def worker_done(self):
         with self.namespace:
